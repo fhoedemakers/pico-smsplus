7c138a9a5d3bbdc737d2ac16c4525fab859a6138
Frank Hoedemakers <fhoedemakers@hotmail.com>
2024-03-17 13:22:59 +0100

    Add build scripts for different configurations

---
+++ buildAll.sh
@@ -0,0 +1,30 @@
+:
+# ====================================================================================
+# PICO-SMSPLUS build all script 
+# Builds the emulator for the default and alternate configuration
+# Binaries are copied to the releases folder
+#   - piconesPlusPimoroniDV.uf2     Pimoroni Pico DV Demo Base
+#   - piconesPlusAdaFruitDVISD.uf2  AdaFruit HDMI and SD Breakout boards
+#   - piconesPlusFeatherDVI.uf2     Adafruit Feather RP2040 DVI + SD Wing
+# ====================================================================================
+#export RETAINSDK=1
+cd `dirname $0` || exit 1
+[ -d releases ] || mkdir releases || exit 1
+#. ./checksdk.sh
+./build.sh
+if [ -f build/piconesPlus.uf2 ] ; then
+	cp build/picosmsPlus.uf2 releases/picosmsPlusimoroniDV.uf2 || exit 1
+fi
+cd `dirname $0` || exit 1
+./build_alternate.sh
+if [ -f build/piconesPlus.uf2 ] ; then
+	cp build/picosmsPlus.uf2 releases/picosmsPlusAdaFruitDVISD.uf2 || exit 1
+fi
+cd `dirname $0` || exit 1
+./build_feather_dvi.sh
+if [ -f build/piconesPlus.uf2 ] ; then
+	cp build/picosmsPlus.uf2 releases/picosmsPlusFeatherDVI.uf2 || exit 1
+fi
+ls -l releases
+#unset RETAINSDK
+#. ./removetmpsdk.sh
