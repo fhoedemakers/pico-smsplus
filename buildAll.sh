2dcf30ba80ad0eb025fe0efcab9ec9e507aafd8d
Frank Hoedemakers <frank.hoedemakers@gmail.com>
2025-04-25 14:38:01 +0200

    added adafruit metro RP2350

--- buildAll.sh
+++ buildAll.sh
@@ -27,13 +27,18 @@ do
 	./bld.sh -c $HWCONFIG -w
 done
 # build for Pico 2 -arm-s
-HWCONFIGS="1 2"
+HWCONFIGS="1 2 5"
 for HWCONFIG in $HWCONFIGS
 do
 	./bld.sh -c $HWCONFIG -2
+	# don't build for w when HWCONFIG=5
+	if [ $HWCONFIG -ne 5 ]; then
+		./bld.sh -c $HWCONFIG -2 -w
+	fi
 done
-# build for Pico 2 -riscv
+# build for Pico 2 -riscv, Metro RP2350 has no risc support because sd card not working
 HWCONFIGS="1 2"
+#HWCONFIGS="1 2 5"
 if [ ! -d $PICO_SDK_PATH/toolchain/RISCV_RPI_2_0_0_2/bin ] ; then
 	echo "RISC-V toolchain not found in $PICO_SDK_PATH/toolchain/RISCV_RPI_2_0_0_2/bin"	
 	echo "To install the RISC-V toolchain, execute \"bld.sh -h\" for instructions"
