1488e1b9c7a9764f1f35570c772889220778497b
Frank Hoedemakers <frank.hoedemakers@gmail.com>
2025-09-14 07:12:02 +0200

    Added new configs

--- buildAll.sh
+++ buildAll.sh
@@ -12,49 +12,55 @@ if ! command -v picotool &> /dev/null
 then
 	echo "picotool could not be found"
 	echo "Please install picotool from https://github.com/raspberrypi/picotool.git" 
-	exit
+	exit 1
+fi
+# check if risc-v toolchain is installed
+if [ ! -d $PICO_SDK_PATH/toolchain/RISCV_RPI_2_0_0_2/bin ] ; then
+	echo "RISC-V toolchain not found in $PICO_SDK_PATH/toolchain/RISCV_RPI_2_0_0_2/bin"	
+	echo "To install the RISC-V toolchain, execute \"bld.sh -h\" for instructions"
+	exit 1
 fi
 # build for Pico
-HWCONFIGS="1 2 3 4 6"
+HWCONFIGS="1 2 3 4 6 10"
 for HWCONFIG in $HWCONFIGS
 do	
 	./bld.sh -c $HWCONFIG || exit 1
 done
 # build for Pico w
-HWCONFIGS="1 2"
+# No pico_w binaries for HWConfig 1 (#132)
+HWCONFIGS="2"  
 for HWCONFIG in $HWCONFIGS
 do	
 	./bld.sh -c $HWCONFIG -w || exit 1
 done
-# build for Pico 2 -arm-s
-HWCONFIGS="1 2 5 6"
+
+# build for Pico 2 (w) -arm-s
+# No pico2_w binaries for HWConfig 1 (#132)
+# no pico_w binaries for HWConfig 2 (#136)
+HWCONFIGS="1 2 5 6 7 8 9 10"
 for HWCONFIG in $HWCONFIGS
 do
 	./bld.sh -c $HWCONFIG -2 || exit 1
-	# don't build for w when HWCONFIG=5 or 6
-	if [[ $HWCONFIG -ne 5 && $HWCONFIG -ne 6 ]]; then
+	# don't build for w when HWCONFIG=1, 5, 6, 7 and 8
+	if [[ $HWCONFIG -eq 2 ]]; then
 		./bld.sh -c $HWCONFIG -2 -w || exit 1
 	fi
 done
-# build for Pico 2 -riscv, Metro RP2350 has no risc support because sd card not working
-HWCONFIGS="1 2 6"
-#HWCONFIGS="1 2 5"
-if [ ! -d $PICO_SDK_PATH/toolchain/RISCV_RPI_2_0_0_2/bin ] ; then
-	echo "RISC-V toolchain not found in $PICO_SDK_PATH/toolchain/RISCV_RPI_2_0_0_2/bin"	
-	echo "To install the RISC-V toolchain, execute \"bld.sh -h\" for instructions"
-else 
-	for HWCONFIG in $HWCONFIGS
-	do
-		./bld.sh -c $HWCONFIG -r -t $PICO_SDK_PATH/toolchain/RISCV_RPI_2_0_0_2/bin || exit 1
-		# don't build for w when HWCONFIG=5 or 6
-		if [[ $HWCONFIG -ne 5 && $HWCONFIG -ne 6 ]]; then
-			./bld.sh -c $HWCONFIG -r -t $PICO_SDK_PATH/toolchain/RISCV_RPI_2_0_0_2/bin -w || exit 1
-		fi
-	done	
-fi
+# build for Pico 2 -riscv
+# No pico2_w binaries for HWConfig 1 (#132)
+# No risc binaries for Metro RP2350 and Fruit Jam (SD card not working)
+HWCONFIGS="1 2 7 9 10"
+for HWCONFIG in $HWCONFIGS
+do
+	./bld.sh -c $HWCONFIG -r -t $PICO_SDK_PATH/toolchain/RISCV_RPI_2_0_0_2/bin || exit 1
+	# don't build for w when HWCONFIG=1 (#132), 5 and 6, 7 and 9
+	if [[ $HWCONFIG -ne 1 && $HWCONFIG -ne 5 && $HWCONFIG -ne 6 && $HWCONFIG -ne 7 && $HWCONFIG -ne 9  && $HWCONFIG -ne 10 ]]; then
+	 	./bld.sh -c $HWCONFIG -r -t $PICO_SDK_PATH/toolchain/RISCV_RPI_2_0_0_2/bin -w || exit 1
+	fi
+done	
 if [ -z "$(ls -A releases)" ]; then
 	echo "No UF2 files found in releases folder"
-	exit
+	exit 1
 fi
 for UF2 in releases/*.uf2
 do
