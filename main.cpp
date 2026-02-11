7d329ee9f50079317e79fe7418f605e96d247aaf
Frank Hoedemakers <frank.hoedemakers@gmail.com>
2026-02-11 17:55:30 +0100

    Refactor audio processing logic for HSTX and external audio handling

--- main.cpp
+++ main.cpp
@@ -1070,27 +1070,20 @@ void in_ram(process)(void)
     {
         processinput(&pdwPad1, &pdwPad2, &pdwSystem, false, nullptr);
         sms_frame(0);
-#if !HSTX
 #if EXT_AUDIO_IS_ENABLED
         if (settings.flags.useExtAudio == 1)
         {
             processaudioPerFrameI2S();
-        }
-        else
-        {
-            processaudioPerFrameDVI();
-        }
-#else
-        processaudioPerFrameDVI();
+        } else
 #endif
-#else
-        if (settings.flags.useExtAudio == 1)
         {
-            processaudioPerFrameI2S();
-        } else {
+#if !HSTX
+            processaudioPerFrameDVI();
+#else
+       
             processaudioPerFrameHSTX();
+#endif 
         }
-#endif // !HSTX
         ProcessAfterFrameIsRendered();
     }
 }
